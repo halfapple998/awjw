1f564fcd5a9433557ad9525a2636111867483682
605649647@qq.com <605649647@qq.com>
2021-03-09 14:10:53 +0800

    1

--- 81053705-8073-11eb-87a8-001c422c66ca/main.py
+++ 81053705-8073-11eb-87a8-001c422c66ca/main.py
@@ -3,16 +3,6 @@ from __future__ import print_function, absolute_import
 from gm.api import *
 import talib
 
-'''
-本策略以SHFE.rb2101为交易标的，根据其一分钟(即60s频度）bar数据建立双均线模型，
-短周期为20，长周期为60，当短期均线由上向下穿越长期均线时做空，
-当短期均线由下向上穿越长期均线时做多,每次开仓前先平掉所持仓位，再开仓。
-注：为了适用于仿真和实盘，在策略中增加了一个“先判断是否平仓成功再开仓”的判断逻辑，以避免出现未平仓成功，可用资金不足的情况。
-回测数据为:SHFE.rb2101的60s频度bar数据
-回测时间为:2020-04-01 09:00:00到2020-05-31 15:00:00
-'''
-
-
 def init(context):
     context.short = 50  # 短周期均线
     context.long = 210  # 长周期均线
@@ -35,22 +25,7 @@ def on_bar(context, bars):
     position_long = context.account().position(symbol=context.symbol, side=1)
     position_short = context.account().position(symbol=context.symbol, side=2)
 
-    if short_avg[-2] > short_avg[-1] and bars[0].close <= short_avg[-1]:
-
-        if not position_long:
-            order_volume(symbol=context.symbol, volume=1, side=OrderSide_Sell, position_effect=PositionEffect_Open,
-                         order_type=OrderType_Market)
-            print(context.symbol, 'kk')
-
-        else:
-            context.open_short = True
-
-            order_volume(symbol=context.symbol, volume=1, side=OrderSide_Sell, position_effect=PositionEffect_Close,
-                         order_type=OrderType_Market)
-            print(context.symbol, 'pd')
-
     if bars[0].close <= min(long_avg[-1], short_avg[-1]) and short_avg[-1] < short_avg[-2]:
-
         if not position_short and not position_long:
             order_volume(symbol=context.symbol, volume=1, side=OrderSide_Sell, position_effect=PositionEffect_Open,
                          order_type=OrderType_Market)
@@ -67,18 +42,12 @@ def on_bar(context, bars):
                 print(context.symbol, 'kk')
 
     elif bars[0].close >= max(long_avg[-1], short_avg[-1]) and short_avg[-1] > short_avg[-2]:
-        if not position_short or not position_long:
+        if position_short:
+            order_volume(symbol=context.symbol, volume=1, side=OrderSide_Buy,
+                         position_effect=PositionEffect_Close, order_type=OrderType_Market)
+            print(context.symbol, 'pk')
+        if not position_long:
             order_volume(symbol=context.symbol, volume=1, side=OrderSide_Buy, position_effect=PositionEffect_Open,
-                         order_type=OrderType_Market)
-            print(context.symbol, 'kd')
-
-        else:
-            if position_short:
-                order_volume(symbol=context.symbol, volume=1, side=OrderSide_Buy,
-                             position_effect=PositionEffect_Close, order_type=OrderType_Market)
-                print(context.symbol, 'pk')
-            if not position_long:
-                order_volume(symbol=context.symbol, volume=1, side=OrderSide_Buy, position_effect=PositionEffect_Open,
                              order_type=OrderType_Market)
                 print(context.symbol, 'kd')
     elif bars[0].close >= short_avg[-1]:
